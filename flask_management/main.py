80f1ea4e1a08f170486dc110f11acb0e42616a7d
huangsong <huangsong@leyantech.com>
2023-05-17 15:36:26 +0800

    update

--- flask_management/main.py
+++ flask_management/main.py
@@ -1,9 +1,16 @@
+import os
+from typing import TypeVar
+
 import typer
+from cookiecutter.exceptions import OutputDirExistsException
+from cookiecutter.main import cookiecutter
+from pydantic.main import BaseModel
 
 from flask_management.constants import PythonVersion
+from flask_management.constants import TEMPLATES_DIR
 from flask_management.context import ProjectContext
-from flask_management.generator import generate_project
 
+ContextType = TypeVar("ContextType", bound=BaseModel)
 app = typer.Typer(
     add_completion=False,
     help="Managing flask projects made easy!",
@@ -11,7 +18,24 @@ app = typer.Typer(
 )
 
 
-@app.command(help="Creates a flask project.")
+def fill_template(template_name: str, context: ContextType):
+    try:
+        cookiecutter(
+            os.path.join(TEMPLATES_DIR, template_name),
+            extra_context=context.dict(),
+            no_input=True,
+        )
+    except OutputDirExistsException:
+        typer.echo(f"Folder '{context.folder_name}' already exists. 😞")
+    else:
+        typer.echo(f"Flask {template_name} created successfully! 🎉")
+
+
+def generate_project(context: ProjectContext):
+    fill_template("project", context)
+
+
+@app.command(help="Creates a flask project with the given name.")
 def startproject(
     name: str,
     python: PythonVersion = typer.Option(PythonVersion.THREE_DOT_EIGHT),
--- flask_management/templates/project/{{ cookiecutter.folder_name }}/{{ cookiecutter.app_name }}/book/__init__.py
+++ flask_management/templates/project/{{ cookiecutter.folder_name }}/{{ cookiecutter.app_name }}/book/__init__.py
@@ -2,7 +2,7 @@ from {{ cookiecutter.app_name }}.signals import after_boot
 
 
 @after_boot.connect
-def init_app(app):
+def init_app(app ):
     from .api import api
     app.register_api(api)
     return
